764e17e697a4f675478a0946bcfc3ecc96e7c50f
Jason Kotchoff <cornflakesuperstar@hotmail.com>
2014-10-27 17:47:53 -0400

    Introduced a ListView which pulls in the titles from: http://www.tidev.io/feed/json

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,16 @@
-function doClick(e) {
-    alert($.label.text);
-}
+var refreshFn = function(){
+  $.listCollection.fetch({
+    success: function(model, response) {
+      console.log("data retrieved");
+    },
+    error: function(model, message){
+      alert("data fetch error: " + message);
+    }
+  });
+};
+
+$.index.addEventListener("open", function(){
+  refreshFn();
+});
 
 $.index.open();
